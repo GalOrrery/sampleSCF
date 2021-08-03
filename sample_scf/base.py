fix broadcasting
Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

sample_scf/base.py
@@ -193,12 +193,15 @@ class SCFSamplerBase:
         -------
         (N, 3) ndarray
         """
+        # coordinates  # TODO! deprecate whan galpy can do ints
+        r = np.asanyarray(r, dtype=float)
+        theta = np.asanyarray(theta, dtype=float)
+        phi = np.asanyarray(phi, dtype=float)
+
         R: NDArray64 = self.rsampler.cdf(r)
         Theta: NDArray64 = self.thetasampler.cdf(theta, r=r)
         Phi: NDArray64 = self.phisampler.cdf(phi, r=r, theta=theta)
-
-        RTP: NDArray64 = np.c_[R, Theta, Phi]
-        return RTP
+        return np.c_[R, Theta, Phi].squeeze()
 
     # /def
 

sample_scf/exact.py
@@ -169,18 +169,20 @@ class SCFThetaSamplerBase(rv_potential):
     @abc.abstractmethod
     def _cdf(self, x: NDArray64, Qls: NDArray64) -> NDArray64:
         xs = np.atleast_1d(x)
-        Qls = np.atleast_2d(Qls)
+        Qls = np.atleast_2d(Qls)  # ({R}, L)
 
         # l = 0
         term0 = 0.5 * (xs + 1.0)  # (T,)
         # l = 1+ : non-symmetry
         factor = 1.0 / (2.0 * Qls[:, 0])  # (R,)
         term1p = np.sum(
-            (Qls[None, :, 1:] * difPls(xs, self._lmax - 1).T[:, None, :]).T,
+            (Qls[:, 1:] * difPls(xs, self._lmax - 1).T).T,
             axis=0,
         )
+        # difPls shape (L, T) -> (T, L)
+        # term1p shape (R/T, L) -> (L, R/T) -> sum -> (R/T,)
 
-        cdf = term0[None, :] + np.nan_to_num(factor[:, None] * term1p)  # (R, T)
+        cdf = term0 + np.nan_to_num(factor * term1p)  # (R/T,)
         return cdf
 
     # /def
@@ -357,27 +359,26 @@ class SCFPhiSamplerBase(rv_potential):
             output.
 
         """
-        Rm, Sm = kw.get("RSms", (self._Rm, self._Sm))  # (len(r), len(theta), L)
+        Rm, Sm = kw.get("RSms", (self._Rm, self._Sm))  # (R/T, L)
 
-        Phis: NDArray64 = np.atleast_1d(phi)[None, None, :, None]  # ({R}, {T}, P, {L})
+        Phis: NDArray64 = np.atleast_1d(phi)[:, None]  # (P, {L})
 
         # l = 0 : spherical symmetry
-        term0: NDArray64 = Phis[..., 0] / (2 * np.pi)  # (1, 1, P)
+        term0: NDArray64 = Phis[..., 0] / (2 * np.pi)  # (1, P)
 
         # l = 1+ : non-symmetry
-        factor = 1 / Rm[:, :, :1]  # R0  (R, T, 1)  # can be inf
-        ms = np.arange(1, self._lmax)[None, None, None, :]  # ({R}, {T}, {P}, L)
+        factor = 1 / Rm[:, 0]  # R0  (R/T,)  # can be inf
+        ms = np.arange(1, self._lmax)[None, :]  # ({R/T/P}, L)
         term1p = np.sum(
-            (Rm[:, :, None, 1:] * np.sin(ms * Phis) + Sm[:, :, None, 1:] * (1 - np.cos(ms * Phis)))
+            (Rm[:, 1:] * np.sin(ms * Phis) + Sm[:, 1:] * (1 - np.cos(ms * Phis)))
             / (2 * np.pi * ms),
             axis=-1,
         )
 
-        cdf: NDArray64 = term0 + np.nan_to_num(factor * term1p)  # (R, T, P)
+        cdf: NDArray64 = term0 + np.nan_to_num(factor * term1p)  # (R/T/P,)
         # 'factor' can be inf and term1p 0 => inf * 0 = nan -> 0
 
-        # return cdf, squeezed of extra dimensions so scalar phi -> scalar
-        return cdf.squeeze()
+        return cdf
 
     # /def
 
@@ -409,7 +410,7 @@ class SCFPhiFixedSampler(SCFPhiSamplerBase):
         # assign fixed r, theta
         self._r, self._theta = r, theta
         # and can compute the associated assymetry measures
-        self._Rm, self._Sm = phiRSms(potential, r, theta)
+        self._Rm, self._Sm = phiRSms(potential, r, theta, grid=False)
 
     # /def
 
@@ -471,7 +472,7 @@ class SCFPhiSampler(SCFPhiSamplerBase):
         ValueError
             If 'r' or 'theta' are None.
         """
-        RSms = phiRSms(self._potential, T.cast(float, r), T.cast(float, theta))
+        RSms = phiRSms(self._potential, T.cast(float, r), T.cast(float, theta), grid=False)
         cdf: NDArray64 = super()._cdf(phi, *args, RSms=RSms)
         return cdf
 

sample_scf/interpolated.py
@@ -32,7 +32,7 @@ from scipy.interpolate import (
 # LOCAL
 from ._typing import NDArray64, RandomLike
 from .base import SCFSamplerBase, rv_potential
-from .utils import _phiRSms, difPls, phiRSms, r_of_zeta, thetaQls, x_of_theta, zeta_of_r
+from .utils import _grid_phiRSms, difPls, phiRSms, r_of_zeta, thetaQls, x_of_theta, zeta_of_r
 
 __all__: T.List[str] = [
     "SCFSampler",
@@ -92,8 +92,8 @@ class SCFSampler(SCFSamplerBase):
 
     Now we can evaluate the CDF
 
-        >>> sampler.cdf(10, np.pi/3, np.pi)
-        array([[0.82666461, 0.9330127 , 0.5       ]])
+        >>> sampler.cdf(10.0, np.pi/3, np.pi)
+        array([0.82666461, 0.9330127 , 0.5       ])
 
     And draw samples
 
@@ -131,7 +131,7 @@ class SCFSampler(SCFSamplerBase):
         # phi Rm, Sm
         # radial and inclination sums
 
-        RSms = _phiRSms(
+        RSms = _grid_phiRSms(
             rhoTilde,
             Acos=potential._Acos,
             Asin=potential._Asin,

sample_scf/tests/test_exact.py
@@ -23,7 +23,7 @@ from sample_scf.utils import difPls, r_of_zeta, thetaQls, x_of_theta
 ##############################################################################
 # PARAMETERS
 
-rgrid = np.concatenate(([0], np.geomspace(1e-1, 1e3, 100)))
+rgrid = np.concatenate(([0], np.geomspace(1e-1, 1e3, 29)))  # same shape as ↓
 tgrid = np.linspace(-np.pi / 2, np.pi / 2, 30)
 pgrid = np.linspace(0, 2 * np.pi, 30)
 
@@ -37,9 +37,17 @@ class _request:
     def __init__(self, param):
         self.param = param
 
+    # /def
+
+
+# /class
+
 
 def getpot(name):
     return next(conftest.potentials.__wrapped__(_request(name)))
+
+
+# /def
 
 
 class Test_SCFSampler(SCFSamplerTestBase):
@@ -92,15 +100,70 @@ class Test_SCFSampler(SCFSamplerTestBase):
     # ===============================================================
     # Plot Tests
 
-    @pytest.mark.skip("TODO!")
-    def test_exact_cdf_plot(self):
-        assert False
+    def test_exact_cdf_plot(self, sampler):
+        """Plot cdf."""
+        kw = dict(marker="o", ms=5, c="k", zorder=5, label="CDF")
+        cdf = sampler.cdf(rgrid, tgrid, pgrid)
+
+        fig = plt.figure(figsize=(15, 3))
+
+        # r
+        ax = fig.add_subplot(
+            131,
+            title=r"$m(\leq r) / m_{tot}$",
+            xlabel="r",
+            ylabel=r"$m(\leq r) / m_{tot}$",
+        )
+        ax.semilogx(rgrid, cdf[:, 0], **kw)
+
+        # theta
+        ax = fig.add_subplot(
+            132,
+            title=r"CDF($\theta$)",
+            xlabel=r"$\theta$",
+            ylabel=r"CDF($\theta$)",
+        )
+        ax.plot(tgrid, cdf[:, 1], **kw)
+
+        # phi
+        ax = fig.add_subplot(
+            133,
+            title=r"CDF($\phi$)",
+            xlabel=r"$\phi$",
+            ylabel=r"CDF($\phi$)",
+        )
+        ax.plot(pgrid, cdf[:, 2], **kw)
+
+        return fig
 
     # /def
 
-    @pytest.mark.skip("TODO!")
-    def test_exact_sampling_plot(self):
-        assert False
+    def test_exact_sampling_plot(self, sampler):
+        """Plot sampling."""
+        samples = sampler.rvs(size=int(1e3), random_state=3)
+
+        fig = plt.figure(figsize=(15, 4))
+
+        ax = fig.add_subplot(
+            131,
+            title=r"$m(\leq r) / m_{tot}$",
+            xlabel="r",
+            ylabel=r"$m(\leq r) / m_{tot}$",
+        )
+        ax.hist(samples.r.value[samples.r < 5e3], log=True, bins=50, density=True)
+
+        ax = fig.add_subplot(
+            132,
+            title=r"CDF($\theta$)",
+            xlabel=r"$\theta$",
+            ylabel=r"CDF($\theta$)",
+        )
+        ax.hist(samples.theta.value, bins=50, density=True)
+
+        ax = fig.add_subplot(133, title=r"CDF($\phi$)", xlabel=r"$\phi$", ylabel=r"CDF($\phi$)")
+        ax.hist(samples.phi.value, bins=50)
+
+        return fig
 
     # /def
 
@@ -384,7 +447,7 @@ class Test_SCFThetaSampler(SCFThetaSamplerTestBase):
             ylabel=r"CDF($\theta$)",
         )
         kw = dict(marker="o", ms=5, c="k", zorder=5, label="CDF")
-        ax.plot(tgrid, sampler.cdf(tgrid, r=10)[0, :], **kw)
+        ax.plot(tgrid, sampler.cdf(tgrid, r=10), **kw)
         ax.axvline(-np.pi / 2, c="tab:blue")
         ax.axhline(sampler.cdf(-np.pi / 2, r=10), c="tab:blue", label=r"$\theta=-\frac{\pi}{2}$")
         ax.axvline(0, c="tab:green")
@@ -400,7 +463,7 @@ class Test_SCFThetaSampler(SCFThetaSamplerTestBase):
             xlabel=r"x$",
             ylabel=r"CDF($x$)",
         )
-        ax.plot(x_of_theta(tgrid), sampler.cdf(tgrid, r=10)[0, :], **kw)
+        ax.plot(x_of_theta(tgrid), sampler.cdf(tgrid, r=10), **kw)
         ax.axvline(x_of_theta(-1), c="tab:blue")
         ax.axhline(sampler.cdf(-1, r=10), c="tab:blue", label=r"$\theta=-\frac{\pi}{2}$")
         ax.axvline(x_of_theta(0), c="tab:green")

sample_scf/utils.py
@@ -227,7 +227,69 @@ def thetaQls(pot: SCFPotential, r: T.Union[float, NDArray64]) -> NDArray64:
 # -------------------------------------------------------------------
 
 
-def _phiRSms(
+def _pnts_phiRSms(
+    rhoTilde: NDArray64,
+    Acos: NDArray64,
+    Asin: NDArray64,
+    r: npt.ArrayLike,
+    theta: npt.ArrayLike,
+) -> T.Tuple[NDArray64, NDArray64]:
+    """Radial and inclination sums for azimuthal weighting factors.
+
+    Parameters
+    ----------
+    rhoTilde: (R, N, L) ndarray
+    Acos, Asin : (N, L, L) ndarray
+    r, theta : float or ndarray[float]
+        With shapes (R,), (T,), respectively.
+
+    Returns
+    -------
+    Rm, Sm : (R, T, L) ndarray
+        Azimuthal weighting factors.
+    """
+    # need r and theta to be arrays. Maintains units.
+    tgrid: NDArray64 = atleast_1d(theta)
+
+    # transform to correct shape for vectorized computation
+    x = x_of_theta(tgrid)  # (R/T,)
+    Xs = x[:, None, None, None]  # (R/T, {N}, {L}, {L})
+
+    # compute the r-dependent coefficient matrix $\tilde{\rho}$
+    nmax, lmax = Acos.shape[:2]
+    RhoT = rhoTilde[:, :, :, None]  # (R/T, N, L, {L})
+
+    # legendre polynomials
+    with warnings.catch_warnings():  # there's a RuntimeWarning to ignore
+        warnings.simplefilter("ignore")
+        lps = lpmn_vec(lmax - 1, lmax - 1, x)[0]  # drop deriv
+
+    PP = np.stack(lps, axis=0).astype(float)[:, None, :, :]
+    # (R/T, {N}, L, L)
+
+    # full R & S matrices
+    RSnlm = RhoT * sqrt(1 - Xs ** 2) * PP  # (R/T, N, L, L)
+
+    # n-sum  # (R/T, N, L, L) -> (R/T, L, L)
+    Rlm = sum(Acos[None, :, :, :] * RSnlm, axis=1)
+    Slm = sum(Asin[None, :, :, :] * RSnlm, axis=1)
+    # fix adding +/- inf -> NaN. happens when r=0.
+    idx = np.all(np.isnan(Rlm[:, 0, :]), axis=-1)
+    Rlm[idx, 0, :] = nan_to_num(Rlm[idx, 0, :])
+    Slm[idx, 0, :] = nan_to_num(Slm[idx, 0, :])
+
+    # m-sum  # (R/T, L)
+    sumidx = range(Rlm.shape[1])
+    Rm = stack([sum(Rlm[:, m:, m], axis=1) for m in sumidx], axis=1)
+    Sm = stack([sum(Slm[:, m:, m], axis=1) for m in sumidx], axis=1)
+
+    return Rm, Sm
+
+
+# /def
+
+
+def _grid_phiRSms(
     rhoTilde: NDArray64,
     Acos: NDArray64,
     Asin: NDArray64,
@@ -293,6 +355,7 @@ def phiRSms(
     pot: SCFPotential,
     r: npt.ArrayLike,
     theta: npt.ArrayLike,
+    grid: bool = True,
 ) -> T.Tuple[NDArray64, NDArray64]:
     r"""Radial and inclination sums for azimuthal weighting factors.
 
@@ -331,7 +394,10 @@ def phiRSms(
     )
 
     # pass to actual calculator, which takes the matrices and r, theta grids.
-    Rm, Sm = _phiRSms(rhoTilde, pot._Acos, pot._Asin, rgrid, tgrid)
+    if grid:
+        Rm, Sm = _grid_phiRSms(rhoTilde, pot._Acos, pot._Asin, rgrid, tgrid)
+    else:
+        Rm, Sm = _pnts_phiRSms(rhoTilde, pot._Acos, pot._Asin, rgrid, tgrid)
     return Rm, Sm
 
 